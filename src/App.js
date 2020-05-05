7fa2468a3093162e0bb9d3d26a9280e6bef80d6d
Demo <currykobejames@gmail.com>
2020-05-05 14:47:35 +0700

    Update footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "./App.css";
 import Navbar from "../src/components/navbar";
 import Carousel from "./components/caurosel";
-import Mvideo from "./components/mvideo";
+import Footer from "./components/footer";
 function App() {
   return (
     <div>
       <Navbar />
       <Carousel />
-      <Mvideo />
+      <Footer />
     </div>
   );
 }
