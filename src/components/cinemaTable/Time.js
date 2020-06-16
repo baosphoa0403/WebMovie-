998341b5472a1ae04b0116418522ca1588456036
Demo <currykobejames@gmail.com>
2020-06-16 11:08:33 +0700

    Styledetail

--- src/components/cinemaTable/Time.js
+++ src/components/cinemaTable/Time.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export default class Time extends Component {
   renderTime = () => {
-    var moment = require('moment');
+    var moment = require("moment");
     if (this.props.item) {
-        return (
-            <div className="time">
-                <a>
-                <span className="info__timeBegin_Home">{moment(this.props.item.ngayChieuGioChieu).format("HH:mm")}</span>
-                </a>
-            </div>
-        );
+      return (
+        <div className="time">
+          <a>
+            <span className="info__timeBegin_Home">
+              {moment(this.props.item.ngayChieuGioChieu).format("HH:mm")}
+            </span>
+          </a>
+        </div>
+      );
     }
   };
-  render() {   
-    return <div className="time_render">
-        {this.renderTime()}
-    </div>;
+  render() {
+    return<div className="time_render">{this.renderTime()}</div>;
   }
 }
