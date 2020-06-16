a2115b0c97cce6620e86c80d9df219375d3032c3
Demo <currykobejames@gmail.com>
2020-06-16 14:16:25 +0700

    Lazy-return

--- src/template/HomeTemplate.js
+++ src/template/HomeTemplate.js
@@ -3,43 +3,52 @@ import Navbar from "../components/navbar";
 import { Route } from "react-router-dom";
 import Loading from "../components/Loading";
 
-class HomeLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true,
-    };
-  }
+// class HomeLayout extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       isLoading: true,
+//     };
+//   }
 
-  render() {
-    return (
+//   render() {
+//     return (
+//       <Fragment>
+//         {this.state.isLoading  ? (<div>{this.renderLoading()}</div>) : ("")}
+//         <Navbar />
+//         {this.props.children}
+//       </Fragment>
+//     );
+//   }
+//   renderLoading = () => {
+//     return (
+//       <div>
+//         <Loading />
+//       </div>
+//     );
+//   };
+
+//   componentWillUnmount() {
+//     //chổ này em hiển thị loading
+//     this.renderLoading();
+//   }
+//   componentDidMount() {
+//     //chổ này em tắt loading nè nó áp dụng tất cả các route luôn
+//     //Chứ h có 100 trang k lẻ vô 100 cái axios viết
+//     this.setState({
+//       isLoading: false,
+//     });
+//   }
+// }
+const HomeLayout  = (props) => {
+  return (
       <Fragment>
-        {this.state.isLoading  ? (<div>{this.renderLoading()}</div>) : ("")}
-        <Navbar />
-        {this.props.children}
+          <Navbar/>
+          {props.children}
       </Fragment>
-    );
+  )
   }
-  renderLoading = () => {
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
-  };
-
-  componentWillUnmount() {
-    //chổ này em hiển thị loading
-    this.renderLoading();
-  }
-  componentDidMount() {
-    //chổ này em tắt loading nè nó áp dụng tất cả các route luôn
-    //Chứ h có 100 trang k lẻ vô 100 cái axios viết
-    this.setState({
-      isLoading: false,
-    });
-  }
-}
+  
 export default function HomeTemplate({ Component, ...props }) {
   return (
     <Route
