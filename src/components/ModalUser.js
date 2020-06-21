a87e6e7cbf70635279e7c6ccca2aada7138b2a1c
bảo <baosphoa0403@gmail.com>
2020-06-21 22:39:23 +0700

    layout user

--- src/components/ModalUser.js
+++ src/components/ModalUser.js
@@ -1,7 +1,37 @@
 import React from "react";
-import Dialog from "@material-ui/core/Dialog";
+import Modal from "@material-ui/core/Modal";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { makeStyles } from "@material-ui/core/styles";
+function rand() {
+  return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+  const top = 50 + rand();
+  const left = 50 + rand();
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: "absolute",
+    width: 800,
+    height: 500,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(5, 3, 3)
+  }
+}));
 export default function ModalUser() {
+  const classes = useStyles();
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
   const [open, setOpen] = React.useState(false);
   const handleClickOpen = () => {
     setOpen(true);
@@ -9,6 +39,14 @@ export default function ModalUser() {
   const handleClose = () => {
     setOpen(false);
   };
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">Text in a modal</h2>
+      <p id="simple-modal-description">
+        Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+      </p>
+    </div>
+  );
   return (
     <div>
       <AccountCircleIcon
@@ -16,20 +54,14 @@ export default function ModalUser() {
         type="button"
         style={{ margin: "4px 10px", fontSize: "30px" }}
       />
-      <Dialog
-        onClose={handleClose}
-        aria-labelledby="customized-dialog-title"
+      <Modal
         open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
       >
-        <iframe
-          width="600"
-          height="315"
-          src="https://www.youtube.com/embed/4Md08rfio6Q"
-          frameborder="0"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        />
-      </Dialog>
+        {body}
+      </Modal>
     </div>
   );
 }
