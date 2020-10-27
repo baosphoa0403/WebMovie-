cb83331390e09a2eb17f6d80b89dc80a9d3d2df8
bảo <baosphoa0403@gmail.com>
2020-10-27 15:02:46 +0700

    fix app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Suspense, lazy } from "react";
+import "./App.css";
+import PageNotFound from "../src/pages/PageNotFound";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import DetailMovie from "./pages/home/DeatailMovie";
+// import TicketBooking from "./pages/home/TicketBooking";
+import { routesAdmin, routesHome } from "./routes";
+import HomeTemplate from "./template/HomeTemplate";
+import AdminTemplate from "./template/AdminTemplate";
+import Admin from "./pages/admin/Admin";
+import Loading from "./components/Loading";
+// const DetailMovie = lazy(() => {
+//   return new Promise((resolve) => {
+//     setTimeout(() => resolve(import("./pages/home/DeatailMovie")), 3000);
+//   });
+// });
+const TicketBooking = lazy(() => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(import("./pages/home/TicketBooking")), 2000);
+  });
+});
+// const PageNotFound = lazy(() => import("../src/pages/PageNotFound"));
 function App() {
+  const showMenuHome = (routes) => {
+    if (routes && routes.length > 0) {
+      return routes.map((item, index) => {
+        return (
+          <HomeTemplate
+            key={index}
+            exact={item.exact}
+            path={item.path}
+            Component={item.component}
+          />
+        );
+      });
+    }
+  };
+  const showMenuAdmin = (routes) => {
+    if (routes && routes.length > 0) {
+      return routes.map((item, index) => {
+        return (
+          <AdminTemplate
+            key={index}
+            exact={item.exact}
+            path={item.path}
+            Component={item.component}
+          />
+        );
+      });
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Suspense fallback={<Loading />}>
+          {showMenuHome(routesHome)}
+          {showMenuAdmin(routesAdmin)}
+          <Route path="/admin" component={Admin} />
+          <Route exact path="/detail/:id" component={DetailMovie} />
+          <Route exact path="/booking/:idLichChieu" component={TicketBooking} />
+        </Suspense>
+        <Route path="" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
